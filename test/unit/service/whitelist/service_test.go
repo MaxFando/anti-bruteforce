62afd58e955e10cfa7f226ec8ec3ac4c48b27b99
mfando <mfando@yandex.ru>
2023-11-23 20:49:00 +0300

    [feature] добавлены тесты

---
+++ test/unit/service/whitelist/service_test.go
@@ -0,0 +1,61 @@
+package whitelist
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/MaxFando/anti-bruteforce/internal/domain/network"
+	"github.com/MaxFando/anti-bruteforce/internal/service/whitelist"
+	mocks "github.com/MaxFando/anti-bruteforce/mocks/service/whitelist"
+	"github.com/MaxFando/anti-bruteforce/pkg/utils"
+)
+
+var (
+	mockStore *mocks.Store
+)
+
+func TestMain(m *testing.M) {
+	utils.InitializeLogger()
+
+	os.Exit(m.Run())
+}
+
+func TestService_AddIP(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		mockStore = new(mocks.Store)
+		s := whitelist.NewService(mockStore)
+
+		payload, _ := network.NewIpNetwork(
+			"192.168.1.1",
+			"255.255.255.0",
+		)
+		prefix, _ := utils.GetPrefix(payload.Ip.String(), payload.Mask.String())
+
+		mockStore.On("Add", mock.Anything, prefix, payload.Mask.String()).Return(nil)
+
+		err := s.AddIP(context.TODO(), payload)
+		assert.NoError(t, err)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		mockStore = new(mocks.Store)
+		s := whitelist.NewService(mockStore)
+
+		payload, _ := network.NewIpNetwork(
+			"192.168.1.1",
+			"255.255.255.0",
+		)
+		prefix, _ := utils.GetPrefix(payload.Ip.String(), payload.Mask.String())
+
+		errMock := errors.New("test")
+		mockStore.On("Add", mock.Anything, prefix, payload.Mask.String()).Return(errMock)
+
+		err := s.AddIP(context.TODO(), payload)
+		assert.ErrorAs(t, err, &errMock)
+	})
+}
